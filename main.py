83765c2f296b6dd9b583b4b1ec7cb177fc0eefee
PetraHrad <petra.hradecky@gmail.com>
2021-03-16 21:07:03 +0100

    new project

---
+++ main.py
@@ -0,0 +1,23 @@
+# list_jmen = ["Jakub", "Petr", "Kamil", "Kamil"]
+# Kolikrat je Kamil uvnitr listu.
+# spojit to co je v listu pomoci "-"
+# Vypsat tento list pozadu
+
+
+list_jmen = ["Jakub", "Petr", "Kamil", "Kamil"]
+print(list_jmen.count("Kamil"))
+
+list_jmen_spojen = "-".join(list_jmen)
+print(list_jmen_spojen)
+
+list_jmen.reverse()
+print(list_jmen)
+
+
+
+
+
+
+
+
+
